f6062596ff8028b7ba587da42c9dd34fb8c458c2
M45 <hilltom@protonmail.com>
2023-05-26 18:37:29 +0300

    Ne rabojet nihuja

--- js/alert.js
+++ js/alert.js
@@ -1,3 +1,15 @@
 "use strict"; 
 // let money = 1000 + 1040 + 416 + 1040 + 312 + 1040; 
 // alert(money);
+
+// function pow(x, n) {
+//   let result = 1;
+
+//   for (let i = 0; i < n; i++) {
+//     result *= x;
+//   }
+//   return result;
+// }
+// alert (pow(2, 5) );
+
+alert ('huj')
--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
 "use strict";
-function loadScript() {
+// function loadScript() {
+//   let script = document.createElement('script');
+//   script.src = 'js/alert.js';
+//   document.head.appendChild(script);
+// }
+
+// main.js
+document.addEventListener('DOMContentLoaded', function() {
+  let testJsButton = document.querySelector('.test-js__button');
+  
+  testJsButton.addEventListener('click', function() {
+    // Загрузка и выполнение скрипта alert.js
+    loadScript('js/alert.js');
+  });
+});
+
+function loadScript(url) {
   let script = document.createElement('script');
-  script.src = 'js/alert.js';
+  script.src = url;
   document.head.appendChild(script);
 }
