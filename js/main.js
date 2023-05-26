69f1d99f8d8bd4c8d809497aa95f8cccca228248
M45 <hilltom@protonmail.com>
2023-05-26 15:07:10 +0300

    Clear html+js

--- js/main.js
+++ js/main.js
@@ -4,20 +4,3 @@ function loadScript() {
   script.src = 'js/alert.js';
   document.head.appendChild(script);
 }
-
-const hoursElem = document.querySelector('.watch__hours-numbers');
-const minutesElem = document.querySelector('.watch__minutes-numbers');
-
-setInterval(() => {
-  // получаем текущее время
-  const now = new Date();
-
-  // обновляем значения элементов часов
-  hoursElem.textContent = padZero(now.getHours());
-  minutesElem.textContent = padZero(now.getMinutes());
-}, 1000);
-
-// функция для добавления ведущего нуля к числу
-function padZero(num) {
-  return num.toString().padStart(2, '0');
-}
