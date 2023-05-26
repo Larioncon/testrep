b5635901744ecff343f8bf9652e7d39ba6f90e2d
M45 <hilltom@protonmail.com>
2023-05-26 14:18:27 +0300

    Fuck this commit

--- js/main.js
+++ js/main.js
@@ -8,16 +8,3 @@ function loadScript() {
 const hoursElem = document.querySelector('.watch__hours-numbers');
 const minutesElem = document.querySelector('.watch__minutes-numbers');
 
-setInterval(() => {
-  // получаем текущее время
-  const now = new Date();
-
-  // обновляем значения элементов часов
-  hoursElem.textContent = padZero(now.getHours());
-  minutesElem.textContent = padZero(now.getMinutes());
-}, 1000);
-
-// функция для добавления ведущего нуля к числу
-function padZero(num) {
-  return num.toString().padStart(2, '0');
-}
