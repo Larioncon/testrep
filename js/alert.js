eb4cc2604f85f5bf8cc50bdd4e9313b20ef8c868
M45 <hilltom@protonmail.com>
2023-05-31 20:45:20 +0300

    less3 done

--- js/alert.js
+++ js/alert.js
@@ -2,3 +2,23 @@
 // let money = 1000 + 1040 + 416 + 1040 + 312 + 1040; 
 // alert(money);
 
+function createIncrement() {
+  
+  let count = 0;
+  
+  function increment() {
+    count++;
+  }
+  function log() {
+    let message = `Count is ${count}`;
+    console.log(message);
+  }
+  return [increment, log];
+}
+
+const [increment, log] = createIncrement();
+
+increment();
+increment();
+increment();
+log();
