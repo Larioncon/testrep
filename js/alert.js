1630082e4ad2186b6767897c30a43d3d3916951a
M45 <hilltom@protonmail.com>
2023-06-13 20:40:56 +0300

    lesson 11 - arrow fn (again)

--- js/alert.js
+++ js/alert.js
@@ -2,25 +2,17 @@
 // let money = 1000 + 1040 + 416 + 1040 + 312 + 1040 + 100; 
 // alert(money);
 
-let lesson10 = 'Привязка контекста к функции';
-//
-function askPassword(ok, fail) {
-  let password = prompt("Password?", '');
-  if (password == "rockstar") ok();
-  else fail();
-}
+let lesson11 = 'Arrow fn';
 
-let user = {
-  name: 'John',
+let group = {
+  title: 'Our Group',
+  students: ['Jogn', 'Pete', 'Alice'],
 
-  login(result) {
-    alert( this.name + (result ? ' logged in' : ' failed to log in') );
+  showList() {
+    this.students.forEach(
+      student => alert(this.title + ': ' + student)
+    );
   }
 };
 
-askPassword(() => user.login(true), () => user.login(false));
-// Или же можно создать частично применённую функцию на основе user.login, 
-// которая использует объект user в качестве контекста и получает
-// соответствующий первый аргумент:
-
-askPassword(user.login.bind(user, true), user.login.bind(user, false));
+group.showList();
