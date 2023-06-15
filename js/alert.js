a5bc5d848c409d7e618aa5bd19a7739c84b10b0c
M45 <hilltom@protonmail.com>
2023-06-16 02:00:03 +0300

    new section - prototypes

--- js/alert.js
+++ js/alert.js
@@ -2,53 +2,5 @@
 // let money = 1000 + 1040 + 416 + 1040 + 312 + 1040 + 100; 
 // alert(money);
 
-let sectionOfLearning = 'Свойства объекта, их конфигурация';
-let lessonSecond = 'Get and set or accessor properties';
-
-let user = {
-  get name() {
-    return this._name;
-  },
-
-  set name(value) {
-    if (value.length < 4) {
-      alert('Name too short!');
-      return;
-    }
-    this._name  = value;
-  }
-};
-
-user.name = "Pet";
-alert(user.name);
-
-function UserF (name, age) {
-  this.name = name;
-  this.age = age;
-}
-
-let johnF = new UserF ('John', 25);
-
-alert(johnF.age);
-
-
-// возраст рассчитывается из текущей даты и дня рождения
-function User (name, birthday) {
-  this.name = name;
-  this.birthday = birthday;
-  
-  Object.defineProperty(this, "age", {
-    get() {
-      let todayYear = new Date().getFullYear();
-      return todayYear - this.birthday.getFullYear();
-    }
-  });
-}
-
-let john = new User('John', new Date(1992, 6, 1));
-let johnJson = JSON.stringify (john);
-alert( john.birthday );
-alert( john.age);
-alert( johnJson );
-//asdasdasd
-
+let newSection = 'prototypes';
+let lesson1 = 'prototype-inheritance';
