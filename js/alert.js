a859410daefe8a94a6c0f35754bbd8515d35e913
M45 <hilltom@protonmail.com>
2023-06-16 01:51:16 +0300

    lesson 2 -  (Свойства - геттеры и сеттеры)

--- js/alert.js
+++ js/alert.js
@@ -3,13 +3,52 @@
 // alert(money);
 
 let sectionOfLearning = 'Свойства объекта, их конфигурация';
-let lessonFirst = 'Флаги и дескрипторы свойств';
+let lessonSecond = 'Get and set or accessor properties';
 
-let user = {};
+let user = {
+  get name() {
+    return this._name;
+  },
 
-Object.defineProperties(user, {
-  name: { value: "John", writable: false },
-  surname: { value: "Smith", writable: false },
-});
+  set name(value) {
+    if (value.length < 4) {
+      alert('Name too short!');
+      return;
+    }
+    this._name  = value;
+  }
+};
 
-let link = 'https://learn.javascript.ru/property-descriptors';
+user.name = "Pet";
+alert(user.name);
+
+function UserF (name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+let johnF = new UserF ('John', 25);
+
+alert(johnF.age);
+
+
+// возраст рассчитывается из текущей даты и дня рождения
+function User (name, birthday) {
+  this.name = name;
+  this.birthday = birthday;
+  
+  Object.defineProperty(this, "age", {
+    get() {
+      let todayYear = new Date().getFullYear();
+      return todayYear - this.birthday.getFullYear();
+    }
+  });
+}
+
+let john = new User('John', new Date(1992, 6, 1));
+let johnJson = JSON.stringify (john);
+alert( john.birthday );
+alert( john.age);
+alert( johnJson );
+//asdasdasd
+
