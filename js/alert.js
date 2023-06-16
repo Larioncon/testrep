c4f232a687b63b43fd79c78896545b2ac92cd812
M45 <hilltom@protonmail.com>
2023-06-16 19:47:08 +0300

    prototypes - lesson 1 - (prototype-inheritance) => done

--- js/alert.js
+++ js/alert.js
@@ -4,3 +4,107 @@
 
 let newSection = 'prototypes';
 let lesson1 = 'prototype-inheritance';
+
+// let animal = {
+//   eats: true
+// };
+// let rabbit = {
+//   jumps: true
+// };
+
+// rabbit.__proto__ = animal;
+
+// alert( rabbit.eats );
+
+let space = " ";
+
+// let animal = {
+//   name: 'animal',
+//   eats: true,
+//   walk() {
+//     alert(`${this.name} walk`);
+//   }
+// };
+
+// let rabbit = {
+//   name: 'rabbit',
+//   jumps: true,
+//   __proto__: animal
+// };
+
+// rabbit.walk();
+
+// let longEar = {
+//   name: 'long ear',
+//   earLength: 10,
+//   __proto__: rabbit
+// };
+
+// longEar.walk();
+// alert(longEar.jumps);
+
+let divider = ' ';
+
+// let animal = {
+//   eats: true
+// };
+// let rabbit = {
+//   jumps: true,
+//   __proto__: animal
+// };
+
+// alert( Object.keys(rabbit));
+// // Object.keys возвращает только собственные ключи
+
+// // for..in проходит и по своим, и по унаследованным ключам
+// for ( let prop in rabbit ) alert(prop);
+//*
+//Если унаследованные свойства нам не нужны,
+//то мы можем отфильтровать их при помощи встроенного метода 
+//obj.hasOwnProperty(key): он возвращает true, 
+//если у obj есть собственное, не унаследованное, свойство с именем key.
+//*
+
+// for ( let prop in rabbit ) {
+//   let isOwn = rabbit.hasOwnProperty(prop);
+
+//   if (isOwn) {
+//     alert(`Our: ${prop}`);
+//   } else {
+//     alert(`Inherited: ${prop}`);
+//   }
+// }
+
+let tasks = ' ';
+
+// let animal = {
+//   jumps: null
+// };
+// let rabbit = {
+//   __proto__: animal,
+//   jumps: true
+// };
+
+// alert( rabbit.jumps); //true
+
+// delete rabbit.jumps;
+
+// alert( rabbit.jumps); //null
+
+// delete animal.jumps;
+
+// alert( rabbit.jumps ); //und
+
+//next task
+
+// let animal = {
+//   eat() {
+//     this.full = true;
+//   }
+// };
+
+// let rabbit = {
+//   __proto__: animal
+// };
+
+// rabbit.eat(); // this = rabbit, rabbit - full.
