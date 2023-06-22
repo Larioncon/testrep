07d19e16df8807bfa6a5e85b351566c69aa3b19c
M45 <hilltom@protonmail.com>
2023-06-22 19:09:04 +0300

    prot - less2 - F.prototype, constructor

--- js/alert.js
+++ js/alert.js
@@ -2,109 +2,40 @@
 // let money = 1000 + 1040 + 416 + 1040 + 312 + 1040 + 100; 
 // alert(money);
 
-let newSection = 'prototypes';
-let lesson1 = 'prototype-inheritance';
+let newSectionPrototypes = 'prototypes';
+let lesson2 = 'F.prototype';
 
 // let animal = {
 //   eats: true
 // };
-// let rabbit = {
-//   jumps: true
-// };
+// function Rabbit(name) {
+//   this.name = name;
+// }
+// Rabbit.prototype = animal;
 
-// rabbit.__proto__ = animal;
+// let rabbit = new Rabbit ('White Rabbit'); //  rabbit.__proto__ == animal
+
 
 // alert( rabbit.eats );
 
-let space = " ";
+//constructor 
 
-// let animal = {
-//   name: 'animal',
-//   eats: true,
-//   walk() {
-//     alert(`${this.name} walk`);
-//   }
-// };
+// function Rabbit () {
 
-// let rabbit = {
-//   name: 'rabbit',
-//   jumps: true,
-//   __proto__: animal
-// };
-
-// rabbit.walk();
-
-// let longEar = {
-//   name: 'long ear',
-//   earLength: 10,
-//   __proto__: rabbit
-// };
-
-// longEar.walk();
-// alert(longEar.jumps);
-
-let divider = ' ';
-
-// let animal = {
-//   eats: true
-// };
-// let rabbit = {
-//   jumps: true,
-//   __proto__: animal
-// };
-
-// alert( Object.keys(rabbit));
-// // Object.keys возвращает только собственные ключи
-
-// // for..in проходит и по своим, и по унаследованным ключам
-// for ( let prop in rabbit ) alert(prop);
-//*
-//Если унаследованные свойства нам не нужны,
-//то мы можем отфильтровать их при помощи встроенного метода 
-//obj.hasOwnProperty(key): он возвращает true, 
-//если у obj есть собственное, не унаследованное, свойство с именем key.
-//*
-
-// for ( let prop in rabbit ) {
-//   let isOwn = rabbit.hasOwnProperty(prop);
-
-//   if (isOwn) {
-//     alert(`Our: ${prop}`);
-//   } else {
-//     alert(`Inherited: ${prop}`);
-//   }
 // }
 
-let tasks = ' ';
+// let rabbit = new Rabbit ();
+// alert( rabbit.constructor == Rabbit );
 
-// let animal = {
-//   jumps: null
-// };
-// let rabbit = {
-//   __proto__: animal,
-//   jumps: true
-// };
+//tasks:
 
-// alert( rabbit.jumps); //true
+function Rabbit () {}
+Rabbit.prototype = {
+  eats: true
+};
 
-// delete rabbit.jumps;
+let rabbit = new Rabbit ();
 
-// alert( rabbit.jumps); //null
+delete Rabbit.prototype.eats;
 
-// delete animal.jumps;
-
-// alert( rabbit.jumps ); //und
-
-//next task
-
-// let animal = {
-//   eat() {
-//     this.full = true;
-//   }
-// };
-
-// let rabbit = {
-//   __proto__: animal
-// };
-
-// rabbit.eat(); // this = rabbit, rabbit - full.
+alert( rabbit.eats );
