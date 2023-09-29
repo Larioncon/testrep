309406dec8387886b4601fd4d5899e0b7f063741
M45 <hilltom@protonmail.com>
2023-09-29 19:05:59 +0300

    mixins

--- js/alert.js
+++ js/alert.js
@@ -1,40 +1,67 @@
 "use strict";
 
 let newSectionClass = "class";
-let lesson6 = "instanceof";
+let lesson6 = "mixins";
 
-// class Rabbit {}
-// let rabbit = new Rabbit ();
-
-// alert ( rabbit instanceof Rabbit );
-
-
-
-// function Rabbit() {
-
-// }
-
-// alert( new Rabbit() instanceof Rabbit );
-
-// let arr = [1, 2, 3];
-
-// alert ( arr instanceof Array );
-// alert ( arr instanceof Object );
-
-// class Animal {
-//   static [Symbol.hasInstance](obj) {
-//     if (obj.canEat) return true;
+// let sayHiMixin = {
+//   sayHi() {
+//     alert(`Hello, ${this.name}`);
+//   };
+//   sayBye() {
+//     alert(`Good bye, ${this.name}.`);
 //   }
+// };
 
+// class User {
+//   constructor(name) {
+//     this.name = name;
+//   }
 // }
 
-// let obj { canEat: true }
 
-// alert(obj instanceof Animal );
+let eventMixin = {
+  on ( eventName, handler) {
+    if (!this._eventHandlers) this._eventHandlers = {};
+    if (!this._eventHandlers[eventName]) {
+      this._eventHandlers[eventName] = [];
+    }
+    this._eventHandlers[eventName].push(handler);
+  },
+
+  off(eventName, handler) {
+    let  handlers = this._eventHandlers?.[eventName];
+    if (!handlers) return;
+    for (let i = 0; i < handlers.length; i++) {
+      if (handlers[1] === handler) {
+        handlers.splice(i--, 1);
+      }
+    }
+  },
+
+  trigger(eventName, ...args) {
+    if (!this._eventHandlers?.[eventName]) {
+      return;
+    }
+
+    this._eventHandlers[eventName].forEach(handler => handler.apply(this, args));
+  }
+};
 
 
 
+class Menu {
+  choose(value) {
+    this.trigger('select', value);
+  }
+}
+
+Object.assign(Menu.prototype, eventMixin);
+
+let menu = new Menu();
+
+menu.on('select', value => alert(`Chosen value: ${value}`));
 
 
+menu.choose('123');
 
-let link = "https://learn.javascript.ru/instanceof";
+let link = "https://learn.javascript.ru/mixins";
